1f6b455901b56947f7137fd3155cf9c7f473847c
dnaresh4 <dnaresh4@gmail.com>
2024-07-11 19:07:58 -0400

    Update LoansController.java

--- src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -57,7 +57,10 @@ public class LoansController {
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createLoan(@RequestParam
                                                       @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
-                                                      String mobileNumber) {
+         
+                                                  String mobileNumber) {
+
+            //resolve the below issue
         int a = 30, b = 0;
         int c = a/b;
         iLoansService.createLoan(mobileNumber);
