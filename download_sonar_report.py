e1868dc45b516e7f4ce4e2703bf66a1a2f5880b8
ndarapan <dnaresh4@gmail.com>
2024-07-10 21:18:02 -0400

    GENAI project commited1-python

--- download_sonar_report.py
+++ download_sonar_report.py
@@ -23,3 +23,5 @@ sonar_token = '273b52a0e0986e3d298f64e0768d31d01a0de09c'
 
 issues = get_sonar_issues(sonar_host, project_key, sonar_token)
 print(issues)
+with open('sonar_issues.json', 'w') as f:
+    json.dump(issues, f, indent=4)
